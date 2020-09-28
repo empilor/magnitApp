433698a221c492d951094f02b0781548f5885d3e
empilor <zelenin_121@mail.ru>
2020-09-28 14:48:23 +0300

    Revert "Feature/create application"

--- src/main/java/com/magnit/magnitapp/app/MagnitXmlApplicationFactory.java
+++
@@ -1,8 +0,0 @@
-package com.magnit.magnitapp.app;
-
-/**
- * Factory for creating MagnitXmlApplication
- */
-public interface MagnitXmlApplicationFactory {
-    MagnitXmlApplication buildMagnitXmlApplication();
-}
